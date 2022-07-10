49391c9a8684b7b4430154b9ed07a2981d46c056
green hand <487751965@qq.com>
2022-07-10 14:18:33 +0800

    添加顶部路由

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -6,18 +6,22 @@
         用两个 template 隔开-->
         <template v-if="user.token">
           <li>
-            <RouterLink to="/member"
-              ><i class="iconfont icon-user"></i>{{ user.account }}</RouterLink
-            >
+            <RouterLink to="/member"><i class="iconfont icon-user"></i>{{ user.account }}</RouterLink>
           </li>
           <li><a href="javascript:;" @click="logout">退出登录</a></li>
         </template>
         <template v-else>
-          <li><RouterLink to="/login">请先登录</RouterLink></li>
+          <li>
+            <RouterLink to="/login">请先登录</RouterLink>
+          </li>
           <li><a href="javascript:;">免费注册</a></li>
         </template>
-        <li><a href="javascript:;">我的订单</a></li>
-        <li><a href="javascript:;">会员中心</a></li>
+        <li>
+          <RouterLink to="/member/order">我的订单</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/member">会员中心</RouterLink>
+        </li>
         <li><a href="javascript:;">帮助中心</a></li>
         <li><a href="javascript:;">关于我们</a></li>
         <li>
@@ -52,27 +56,32 @@ export default {
 <style scoped lang="less">
 .app-topnav {
   background: #333;
+
   ul {
     display: flex;
     height: 53px;
     justify-content: flex-end;
     align-items: center;
+
     li {
       a {
         padding: 0 15px;
         color: #cdcdcd;
         line-height: 1;
         display: inline-block;
+
         i {
           font-size: 14px;
           margin-right: 2px;
         }
+
         &:hover {
           color: @xtxColor;
         }
       }
+
       // 浏览器对 css 的解析也是顺序的，先找到第一个 li ，然后 ~ 符号就是对后面的兄弟  li 节点进行左边竖线的设置
-      ~ li {
+      ~li {
         a {
           border-left: 2px solid #666;
         }
